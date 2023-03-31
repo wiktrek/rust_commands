ed36f0afde3a198a8971cd6638634b412bbd6c33
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-03-31 22:48:18 +0200

    .

--- src/main.rs
+++ src/main.rs
@@ -8,19 +8,8 @@ fn main() {
     .read_line(&mut option).expect("Couldn’t read from stdin");
     let replace_option = option.replace("\r\n", "");
     match replace_option.as_str() {
-    "web" => web(),
-    "info" => info(),
+    "web" => options::web(),
+    "info" => options::info(),
     _ => return println!("Invalid option")
     }
-}
-fn web() {
-    let mut option = String::new();
-    println!("Enter the name from your config (configs/web.json)");
-    io::stdin()
-    .read_line(&mut option).expect("Couldn’t read from stdin");
-    let replace_option = option.replace("\r\n", "");
-options::web(replace_option)
-}
-fn info() {
-    options::info()
 }
--- src/options/web.rs
+++ src/options/web.rs
@@ -1,5 +1,6 @@
 use std::{path::Path};
 use std::fs;
+use std::io;
 use serde::{Serialize, Deserialize};
 use serde_json;
 use webbrowser;
@@ -8,17 +9,27 @@ struct Links {
     name: String,
     link: String,
 }
+pub fn web(){
+    let mut option = String::new();
+    println!("Enter the name from your config (configs/web.json)");
+    io::stdin()
+    .read_line(&mut option).expect("Couldn’t read from stdin");
+    let replace_option = option.replace("\r\n", "");
+    if open_link(replace_option).is_ok() {
+        println!("\n")
+    }    
+}
 
-pub fn web(link: String) {
+pub fn open_link(link: String) -> Result<(), std::io::Error> {
     let path = Path::new("src/configs/web.json");
     let file = fs::File::open(path).expect("failed to open");
 
     let read_to_json:Vec<Links> = serde_json::from_reader(&file).expect("error while reading or parsing");
-    println!("{:?}", read_to_json);
     for l in read_to_json {
         if link == l.name {
             let result = webbrowser::open(l.link.as_str());
-        println!("{:?}", result);
+            return result
         }
     }
+    Ok(())
 }
