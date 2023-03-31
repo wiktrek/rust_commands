8bcfd6f0fe6b3f34273a335874311bedbeb87074
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-03-31 18:05:34 +0200

    configs/options

--- src/options/info.rs
+++ src/options/info.rs
@@ -1,4 +1,18 @@
-pub fn info() {
-    println!("options:")
+use std::{path::Path};
+use std::fs;
+use serde::{Serialize, Deserialize};
+#[derive(Serialize, Deserialize, Debug)]
+struct Options {
+    name: String,
+    description: String,
+}
 
+pub fn info() {
+    let path = Path::new("src/configs/options.json");
+    let file = fs::File::open(path).expect("failed to open");
+
+    let read_to_json:Vec<Options> = serde_json::from_reader(&file).expect("error while reading or parsing");
+    for option in read_to_json{
+        println!("name: {}, description: {} \n", option.name, option.description)
+    }
 }
