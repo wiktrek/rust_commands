8155a34a2fc4f623ce7a5fb35822a089ce8f488f
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-04-01 17:15:30 +0200

    cmds

--- src/options/cmds.rs
+++ src/options/cmds.rs
@@ -1,3 +1,49 @@
+use std::{path::Path};
+use std::fs;
+use std::io;
+use serde::{Serialize, Deserialize};
+use std::process::Command;
+#[derive(Serialize, Deserialize, Debug)]
+struct Cmds {
+    name: String,
+    cmd: String,
+    arg: String,
+}
 pub fn cmds() {
-    println!("cmds!")
+    println!("Enter name of your cmd from your config");
+    let mut option = String::new();
+    io::stdin()
+    .read_line(&mut option).expect("Couldn’t read from stdin");
+    let cmd = option.replace("\r\n", "");
+    cmd_run(cmd)
+}
+fn check_bash() -> String{
+    println!("bash or cmd");
+    let mut option = String::new();
+    io::stdin()
+    .read_line(&mut option).expect("Couldn’t read from stdin");
+if option != "bash" || option != "cmd" {
+    return "err".to_string();
+} else {
+return option.to_string()
+}
+
+}
+fn cmd_run(cmd: String) {
+    let path = Path::new("src/configs/options.json");
+    let file = fs::File::open(path).expect("failed to open");
+
+    let read_to_json:Vec<Cmds> = serde_json::from_reader(&file).expect("error while reading or parsing");
+    for cmd_option in read_to_json{
+        let a = check_bash();
+        if a == "err" {
+            return println!("error")
+    }
+    let output = Command::new("echo")
+        .arg("Hello world")
+        .output()
+        .expect("Failed to execute command");
+ assert_eq!(b"cmd result:\n", output.stdout.as_slice());
+
+    }
 }
